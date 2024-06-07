1bc42d0495ea0aba68a2aaf23423f1db463bca2e
divyanshraj467 <divyanshraj1@gmail.com>
2024-06-07 13:56:23 +0530

    office commit

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -2,5 +2,11 @@
 
 public class Comment
 {
+    public int ID { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
+    public int? StockId { get; set; }
+    public Stocks? Stock { get; set; }
 
 }
---
+++ api/Models/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace api;
+
+public class Stocks
+{
+    public int ID { get; set; }
+    public string Symbol { get; set; } = string.Empty;
+    public string Companyname { get; set; } = string.Empty;
+    [Column(TypeName = "decimal(18,2)")]
+    public string Purchase { get; set; } = string.Empty;
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Dividend { get; set; } = decimal.Zero;
+    public string  Industry { get; set; } = string.Empty;
+    public long MarketCap { get; set; }
+    public List<Comment> Comments { get; set; } = new List<Comment>
+}
